9df0900a17f371b2bee71e0ffd5411360e5d7697
Albert Chen <achen17@stevens.edu>
2021-04-27 16:44:58 -0400

    add readme and process command line inputs

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
+const phonetics = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
+
 function main() {
-    console.log("Hello world");
-  }
+    //process command line inputs
+    let inputs = [];
+    for (let i = 2; i < process.argv.length; i++){
+        inputs.push(process.argv[i]);
+    }
+
+    let answers = [];
+    for (let i = 0; i < inputs.length; i++){
+        let string = '';
+        let num = inputs[i];
+        for (let j = 0; j < num.length; j++){
+            string += phonetics[num[j]];
+        }
+        answers.push(string);
+    }
+    console.log(answers.toString());
+    
+}
   
 if (require.main === module) {
     main();
